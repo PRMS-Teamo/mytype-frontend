4c9d151c437d843a90a5a78296ef3680a494184b
heeyoung123 <heeyoung321@naver.com>
2025-05-22 00:34:18 +0900

    chore : 테일윈드 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css'
-
 const App = ()=>{
   return(
-    <div>ㅇㅇ</div>
+    <h1 className="text-3xl font-bold text-blue-500">Hi</h1>
   )
 }
 
-export default App
+export default App;
