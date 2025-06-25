a2e5c6e0bea97f9a83bffaaff9fb890b204168e5
heeyoung123 <heeyoung321@naver.com>
2025-06-26 04:46:02 +0900

    [250626] fix :라우터 충돌 해결

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Intro from "./pages/Intro";
 import Home from "./pages/Home";
-<<<<<<< feat/#11-Header
+
 import TeamList from "./pages/Team/TeamList";
 import TeamDetail from "./pages/Team/TeamDetail";
 import TeamWrite from "./pages/Team/TeamWrite";
@@ -10,23 +10,24 @@ import TeammateList from "./pages/Teammate/TeammateList";
 import TeammateDetail from "./pages/Teammate/TeammateDetail";
 import TeammateWrite from "./pages/Teammate/TeammateWrite";
 
-
-=======
 import MyPage from "./pages/MyPage";
-// import{ URL} from "./constants/url/url"
->>>>>>> develop
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Intro />,
   },
   {
-<<<<<<< feat/#11-Header
     element: <Layout />, 
     children: [
       {
         path: "home",
         element: <Home />,
+      },
+      {
+        path: "/mypage",
+        element: <MyPage />,
+
       },
       {
         path: "team",
@@ -43,21 +44,7 @@ const router = createBrowserRouter([
           { path: ":id", element: <TeammateDetail /> },
           { path: "write", element: <TeammateWrite /> },
         ],
-=======
-    element: <Layout />,
-    children: [
-      {
-        path: "/home",
-        element: <Home />,
-      },
-      {
-        path: "/mypage",
-        element: <MyPage />,
->>>>>>> develop
-      },
-    ],
-  },
-
+       },]}
 ]);
 
 export default router;
