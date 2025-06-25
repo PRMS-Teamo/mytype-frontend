e472117ae4e1a8b653b10182892a4e422e9517e8
신혜민 <shinhyemin225@gmail.com>
2025-06-25 14:26:08 +0900

    feat: 헤더 컴포넌트 추가

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,19 @@
 import Sidebar from "../Sidebar";
+import Header from "../Header";
+import KakaoLoginModal from "../KakaoLoginModal";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar />
-      <main className="flex-1 overflow-auto">
-        <Outlet />
-      </main>
+      <div className="flex-1 flex flex-col overflow-auto">
+        <Header />
+        <main className="flex-1 overflow-auto px-6 py-4">
+          <Outlet />
+        </main>
+      </div>
+      <KakaoLoginModal />
     </div>
   );
 }
