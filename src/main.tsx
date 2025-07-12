4e1cca5f96aa33886a6e00744cfa6123d6a94f90
heeyoung123 <heeyoung321@naver.com>
2025-07-12 14:38:59 +0900

    [250711] feat: lazy,suspense 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
+
 async function prepare() {
   if (import.meta.env.MODE === 'development') {
     const { worker } = await import('./mock/browser');
