374d07814186457dc2b63b81b7cf404fc0f3d81f
scorchedrice <wldnd2977@gmail.com>
2025-07-15 17:44:48 +0900

    feat: 내 팀 정보

--- src/hooks/useTeamInfo.ts
+++ src/hooks/useTeamInfo.ts
@@ -8,17 +8,17 @@ const useTeamInfo = () => {
 
   useEffect(() => {
     // userInfo가 로딩되지 않았다면 아무 것도 안 함
-    if (!userInfo?.positionId) {
-      console.log(userInfo)
-      console.warn("유저 정보 또는 positionId 없음");
+    if (userInfo === null) {
+      return null;
+    }
+    if (!userInfo.isJoined) {
       return;
     }
-
     const getTeamInfo = async () => {
       try {
         const accessToken = localStorage.getItem("accessToken");
         const res = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/teams/me/${userInfo.positionId}`,
+          `${import.meta.env.VITE_BACKEND_URL}/teams/me/${userInfo?.positionId}`,
           {
             headers: {
               authorization: `Bearer ${accessToken}`,
--- src/pages/MyTeamInfo/MyTeamInfo.tsx
+++ src/pages/MyTeamInfo/MyTeamInfo.tsx
@@ -11,20 +11,32 @@ const MyTeamInfo = () => {
     "김종현",
     "한지웅",
     "조민우",
-  ]; // TODO : 팀원 정보를 받아오는 API 로직 추가가 필요함.
+  ]; // TODO : 팀원 정보를 받아오는 API 로직 추가가 필요함. 정보가 온다면 하단처럼 옴.
+  // const teamMateList = myTeamData.teamPositions.teamUsers;
   return (
     <div className="w-full">
-      <Label children="팀명"/>
-      <ListItem content="마이타입" buttonContent="팀 나가기" onClick={() => console.log('team out')}/>
+      {
+        myTeamData === null ?
+          <div>소속된 팀이 없습니다.</div> :
+          <><Label children="팀명"/>
+            <ListItem content="마이타입" buttonContent="팀 나가기" onClick={() => console.log('team out')}/></>
+      }
       <br/>
       <hr/>
       <br/>
-      <Label>팀원 정보</Label>
-      {teamMateList.map((item) => {
-        return (
-          <ListItem content={item} buttonContent="메시지 보내기" onClick={() => console.log('test')}/>
-        )
-      })}
+      {
+        myTeamData === null || teamMateList.length === 0 ?
+          <div>정보가 없습니다.</div> :
+          <>
+            <Label>팀원 정보</Label>
+            {teamMateList.map((item) => {
+              return (
+                <ListItem content={item} buttonContent="메시지 보내기" onClick={() => console.log('test')}/>
+              )
+            })}
+          </>
+      }
+
     </div>
   )
 }
