6a09a89eaec361d627b00886b4f61a36e8ebc057
scorchedrice <wldnd2977@gmail.com>
2025-07-19 22:05:10 +0900

    fix: 로드 초기, selected 색상이 반영되지 않음.

--- src/components/TechStack/TechStackLabel/TechStackLabel.tsx
+++ src/components/TechStack/TechStackLabel/TechStackLabel.tsx
@@ -10,13 +10,16 @@ const TechStackLabel = ({ stacks, selected, onToggle }: Props) => {
 	return (
 		<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-8 gap-2">
 			{stacks.map((stack) => {
-				const isActive = selected.includes(stack);
+				console.log("selected", selected);
+				console.log("stack", stack)
+				const isActive = selected.some((s) => s.stackId === stack.stackId);
 				return (
 					<div
 						key={stack.stackId}
 						onClick={() => onToggle(stack)}
 						className={`w-full h-10 flex items-center justify-center rounded-full border text-sm cursor-pointer transition
               ${isActive ? "bg-main text-white border-main" : "bg-white text-black border-main"}`}
+						style={{ backgroundColor: isActive ? "#5932EA" : "#FFFFFF" }}
 					>
 						{stack.stackName}
 					</div>
